fde664cf3abec890ff1b38e4acb28478dddc6fe9
Antonio <antoniogamiz10@gmail.com>
2017-12-13 22:39:16 +0100

    requisitos implementados (etiquetas añadidas al spider)

--- Posts/Posts/spiders/post_spider.py
+++ Posts/Posts/spiders/post_spider.py
@@ -11,16 +11,17 @@ class PostsSpider(scrapy.Spider):
         for href in response.xpath("//article/section/header/h2/a"):    # Analizamos cada post de una página.
             yield response.follow(href, self.parse_post)
 
-        next_page = response.xpath("/html/body/div[1]/div[2]/div[3]/div/div[1]/nav/ul/li/span/a/@href").extract_first() # Avanzamos a la siguiente página.
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        # next_page = response.xpath("/html/body/div[1]/div[2]/div[3]/div/div[1]/nav/ul/li/span/a/@href").extract_first() # Avanzamos a la siguiente página.
+        # if next_page is not None:
+        #     yield response.follow(next_page, self.parse)
 
     def parse_post(self, response):
         yield {
             "title" : response.xpath("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/header/h1/text()").extract(),
             "author" : response.xpath("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/header/div/span/span/a/text()").extract(),
-            "contenido" : response.xpath("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/section/p").extract(),
-            "categorias" : response.xpath("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/header/div/a/text()").extract(),
+            # "contenido" : response.xpath("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/section/p").extract(),
+            # "categorias" : response.xpath("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/header/div/a/text()").extract(),
+            "etiquetas" : response.xpath("/html/body/div[1]/div[2]/div[2]/div/div[1]/article/header/div/a[@class='btn btn-mini']/text()").extract()
         }
 
 
